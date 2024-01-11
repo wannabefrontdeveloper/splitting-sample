ed2ed77f67008115a5f2c5586a0c13443c4420d1
wannabefrontdeveloper <giwonk@naver.com>
2024-01-11 09:32:41 +0900

    코드 수정 Loadable Components를 사용해서 코드 스플리팅을 하도록 변경하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { useState, Suspense } from "react";
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-const SplitMe = React.lazy(() => import("./SplitMe"));
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"));
 
 function App() {
   const [visible, setVisible] = useState(false);
@@ -14,9 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
