eea0a6d98fa833d8d549ad3d390357fb15b27832
wannabefrontdeveloper <giwonk@naver.com>
2024-01-11 09:26:34 +0900

    코드수정 1. React.lazy와 Suspense를 사용하였습니다. 2. 다시 함수 컴포넌트로 전환하였습니다. 3. 단순히 SplitMe 컴포넌트의 가시성을 의미하는 visible이라는 상태만 업데이트하여 코드 스플리팅된 컴포넌트를 보여주도록 하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import { Component } from "react";
+import React from "react";
+import { useState, Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+const SplitMe = React.lazy(() => import("./SplitMe"));
 
-class App extends Component {
-  state = {
-    SplitMe: null,
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   };
-  handleClick = async () => {
-    const loadedModule = await import("./SplitMe");
-    this.setState({
-      SplitMe: loadedModule.default,
-    });
-  };
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>Hello React!</p>
-          {SplitMe && <SplitMe />}
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
 }
 
 export default App;
