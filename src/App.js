04700131d716e03bb305fd6869eb41d5b5f7a53b
wannabefrontdeveloper <giwonk@naver.com>
2024-01-11 09:09:27 +0900

    코드 수정 import를 상단에서 하지않고 import() 함수 형태로 메서드 안에서 사용하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
-import notify from "./notify";
 
 function App() {
   const onClick = () => {
-    notify();
+    import("./notify").then((result) => result.default());
   };
   return (
     <div className="App">
